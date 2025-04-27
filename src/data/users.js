d468b3430b02b8808f9f894ba71a29fd58198a70
Shad-islam <m.shadislam01@gmail.com>
2025-04-27 21:07:36 +0600

    Add dummy data

---
+++ src/data/users.js
@@ -0,0 +1,14 @@
+const users = [
+  {
+    name: "John Doe",
+    phone: "1234567890",
+    password: "password123",
+  },
+  {
+    name: "Jane Smith",
+    phone: "0987654321",
+    password: "password456",
+  },
+];
+
+export default users;
