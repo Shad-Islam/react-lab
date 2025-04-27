14e6ac99d02bc12a2eb6c7f5e80c87234ef7ad2a
Shad-islam <m.shadislam01@gmail.com>
2025-04-27 20:31:17 +0600

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center text-green-500">
-        Flate m8
-      </h1>
-      <p className="text-lg text-center">Welcome to the app!</p>
-    </>
+    <div className="flex flex-col justify-between min-h-screen">
+      <div>
+        <h1 className="text-3xl font-bold underline text-center text-green-500">
+          Hello dev
+        </h1>
+        <p className="text-lg text-center">Welcome to the app!</p>
+      </div>
+      <footer className="text-sm text-center text-gray-500">
+        © {new Date().getFullYear()} Shad.dev. All rights reserved.
+      </footer>
+    </div>
   );
 }
 
