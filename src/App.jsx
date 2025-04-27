cc875b9ae03b968ae43caa3778c739fcf28ccf9a
Shad-islam <m.shadislam01@gmail.com>
2025-04-28 02:14:02 +0600

    Create dummy data and install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/loginPage/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
-    <div>
-      <Login />
-      {/* <Dashboard /> */}
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/loginPage/Login.jsx
+++ src/pages/loginPage/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Login() {
+    
   return (
     <>
       <section className="flex flex-col items-center justify-center h-screen ">
