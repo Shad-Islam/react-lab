e44a3683ae772ac8dd30aeb6bdbfe241cd87a470
Shad-islam <m.shadislam01@gmail.com>
2025-04-28 19:37:37 +0600

    Add 'not found' component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/loginPage/Login";
 import Dashboard from "./pages/dashboard/Dashboard";
+import NotFound from "./pages/not found/NotFound";
 
 function App() {
   const isLoggedIn = localStorage.getItem("user") !== null;
@@ -10,8 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         {isLoggedIn && <Route path="/dashboard" element={<Dashboard />} />}
-
-        
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
